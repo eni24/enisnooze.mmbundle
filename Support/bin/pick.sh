92c1e27a9ff57b0991e5fa30bc2503a0a9bfc9a2
eni24 <awm@posteo.de>
2020-12-01 17:31:31 +0100

    Fix Applescript

--- Support/bin/pick.sh
+++ Support/bin/pick.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 DEF=""
+DIALOGUE_TEXT="Enter Snooze time. Options are:\n- <N>d: <N>days\n- <N>w: <N>weeks\n- <N>m: <N>months\n- yyyy-mm-dd \n- mon, tue, ..., sun\n- xx: unsnooze email"
 osascript <<END
-tell application "System Events"
-	activate
-	set theDate to the text returned of (display dialog "Enter Snooze time. Options are:\n- <N>d: <N>days\n- <N>w: <N>weeks\n- <N>m: <N>months\n- yyyy-mm-dd \n- mon, tue, ..., sun\n- xx: unsnooze email" default answer "${DEF}" with title "Snooze Message" buttons {"Cancel", "Snooze"} default button "Snooze" cancel button "Cancel")
-	return theDate
-end tell
+set theDate to the text returned of (display dialog "${DIALOGUE_TEXT}"  default answer "${DEF}")
+return theDate
 END
